f343a01a2bcc91169894beffe51cdcd03f536c90
R3D4L1T <redalit@proton.me>
2026-06-01 11:33:42 -0500

    change astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,16 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import Icons from 'unplugin-icons/vite';
 
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
 export default defineConfig({
+  output: 'static',
+
   vite: {
     plugins: [
       tailwindcss(),
       Icons({
         compiler: 'astro',
       }),
-    ]
+    ],
   },
-
-  adapter: cloudflare({
-    imageService: 'passthrough',
-  })
 });
