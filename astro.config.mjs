27a8479c3554d42c0531f2fc08939c0f9475348c
R3D4L1T <redalit@proton.me>
2026-09-01 09:37:31 -0500

    fix deploy pnpm to npm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import Icons from 'unplugin-icons/vite';
+import cloudflare from '@astrojs/cloudflare'
 
 export default defineConfig({
   output: 'static',
+  adapter: cloudflare(),
 
   vite: {
     plugins: [
